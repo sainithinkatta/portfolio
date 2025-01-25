c819473dce5401566a2e56afd5b59ddd587fda49
sainithinkatta <sainithinkatta24@gmail.com>
2025-01-24 21:27:43 -0600

    re-indented experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -4,8 +4,14 @@ import experiences from '../../data/experience.json';
 
 export const Experience = () => {
   return (
-    <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Experience</h2>
+    <section 
+      id="experience"
+      className={styles.container} 
+    >
+      <h2 className={styles.title}>
+        Experience
+      </h2>
+
       <div className={styles.content}>
         <img
           src={getImageUrl("experience/exp_icon.png")}
@@ -18,11 +24,16 @@ export const Experience = () => {
             experiences.map((experience, index) => (
               <li key={index} className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>
-                  <h3 className={styles.aboutItemTitle}>{experience.role} - {experience.organisation}</h3>
+                  <h3 className={styles.aboutItemTitle}>
+                    {experience.role} - {experience.organisation}
+                  </h3>
+
                   <ul>
-                    {experience.experiences.map((exp, expIndex) => (
-                      <li key={expIndex}>{exp}</li>
-                    ))}
+                    {
+                      experience.experiences.map((exp, expIndex) => (
+                        <li key={expIndex}>{exp}</li>
+                      ))
+                    }
                   </ul>
                 </div>
               </li>
