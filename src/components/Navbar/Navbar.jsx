6c7e2292898e91c88c6185b2f3f888e6809fe0cb
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-18 13:25:34 -0600

    removed unused code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-
 import styles from "./Navbar.module.css";
-import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={styles.navbar}>
@@ -13,24 +9,7 @@ export const Navbar = () => {
       </a>
 
       <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
-          alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        />
-
-        <ul
-          className={
-            `${styles.menuItems} 
-            ${menuOpen && styles.menuOpen}`
-          }
-          onClick={() => setMenuOpen(false)}
-        >
+        <ul className={styles.menuItems}>
           <li>
             <a href="#skills">Skills</a>
           </li>
