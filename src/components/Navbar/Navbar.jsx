2ea9cd9bedfd11edae3653ea6bd6c026cf1dfdb9
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-27 16:09:08 -0600

    resolved menu list mobile issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -9,7 +15,15 @@ export const Navbar = () => {
       </a>
 
       <div className={styles.menu}>
-        <ul className={styles.menuItems}>
+        <div className={styles.menuBtn} onClick={toggleMenu}>
+          <div className={styles.hamburger}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        
+        <ul className={`${styles.menuItems} ${menuOpen ? styles.menuOpen : ""}`}>
           <li>
             <a href="#skills">Skills</a>
           </li>
